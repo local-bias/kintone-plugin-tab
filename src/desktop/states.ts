e1f7fa0538543039f7f49e0924b4d47a2fda8ca0
Ribbit <echo07200503@gmail.com>
2022-07-01 17:21:31 +0900

    ⚡ レンダリングコストの削減

--- src/desktop/states.ts
+++ src/desktop/states.ts
@@ -4,3 +4,8 @@ export const pluginConfigState = atom<kintone.plugin.Storage | null>({
   key: 'pluginConfigState',
   default: null,
 });
+
+export const tabIndexState = atom<number>({
+  key: 'tabIndexState',
+  default: 0,
+});
--- src/desktop/tab.tsx
+++ src/desktop/tab.tsx
@@ -1,34 +1,67 @@
-import React, { useState, FC, FCX } from 'react';
+import React, { FC, FCX } from 'react';
 import styled from '@emotion/styled';
-import { Box, Tab, Tabs } from '@mui/material';
-import { useRecoilValue } from 'recoil';
-import { pluginConfigState } from './states';
+import { Tab, Tabs } from '@mui/material';
+import { useRecoilCallback, useRecoilValue } from 'recoil';
+import { pluginConfigState, tabIndexState } from './states';
 import { getCurrentRecord, setFieldShown } from '@common/kintone';
 
-type ContainerProps = Readonly<{}>;
-type Props = ContainerProps &
-  Readonly<{
-    index: number;
-    onTabChange: (_: any, value: number) => void;
-    storage: kintone.plugin.Storage | null;
-  }>;
+const TabComponent: FC = () => {
+  const storage = useRecoilValue(pluginConfigState)!;
+  const tabIndex = useRecoilValue(tabIndexState);
 
-const Component: FCX<Props> = ({ className, index, onTabChange, storage }) => (
+  const onTabChange = useRecoilCallback(
+    ({ set, snapshot }) =>
+      async (_: any, index: number) => {
+        set(tabIndexState, index);
+        const storage = await snapshot.getPromise(pluginConfigState);
+
+        if (!storage || !storage.conditions[index]) {
+          return;
+        }
+
+        const { record } = getCurrentRecord();
+
+        Object.keys(record).forEach((code) => {
+          setFieldShown(code, true);
+        });
+
+        const condition = storage.conditions[index];
+
+        if (condition.displayMode === 'sub') {
+          condition.fields.map((code) => {
+            setFieldShown(code, false);
+          });
+        } else {
+          Object.keys(record).forEach((code) => {
+            setFieldShown(code, condition.fields.includes(code));
+          });
+        }
+      },
+    []
+  );
+
+  if (!storage) {
+    return <div></div>;
+  }
+
+  return (
+    <Tabs orientation='vertical' variant='scrollable' value={tabIndex} onChange={onTabChange}>
+      {storage.conditions.map((condition, i) => (
+        <Tab key={i} label={condition.tabName} />
+      ))}
+    </Tabs>
+  );
+};
+
+const Container: FCX = ({ className }) => (
   <div {...{ className }}>
     <div>
-      {!storage && <div></div>}
-      {!!storage && (
-        <Tabs orientation='vertical' variant='scrollable' value={index} onChange={onTabChange}>
-          {storage.conditions.map((condition, i) => (
-            <Tab key={i} label={condition.tabName} />
-          ))}
-        </Tabs>
-      )}
+      <TabComponent />
     </div>
   </div>
 );
 
-const StyledComponent = styled(Component)`
+const StyledContainer = styled(Container)`
   flex: 1;
   background-color: #fff;
   display: flex;
@@ -39,40 +72,9 @@ const StyledComponent = styled(Component)`
     position: sticky;
     top: 130px;
     min-width: 150px;
+    max-width: 200px;
     height: calc(100vh - 130px);
   }
 `;
 
-const Container: FC<ContainerProps> = () => {
-  const [index, setIndex] = useState(0);
-  const storage = useRecoilValue(pluginConfigState);
-
-  const onTabChange = (_: any, value: number) => {
-    setIndex(value);
-    if (!storage || !storage.conditions[value]) {
-      return;
-    }
-
-    const { record } = getCurrentRecord();
-
-    Object.keys(record).forEach((code) => {
-      setFieldShown(code, true);
-    });
-
-    const condition = storage.conditions[value];
-
-    if (condition.displayMode === 'sub') {
-      condition.fields.map((code) => {
-        setFieldShown(code, false);
-      });
-    } else {
-      Object.keys(record).forEach((code) => {
-        setFieldShown(code, condition.fields.includes(code));
-      });
-    }
-  };
-
-  return <StyledComponent {...{ index, onTabChange, storage }} />;
-};
-
-export default Container;
+export default StyledContainer;
