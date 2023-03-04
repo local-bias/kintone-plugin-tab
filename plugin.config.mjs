43d37518696807c999940bda8bf9a522ebea8994
Ribbit <echo07200503@gmail.com>
2023-03-04 11:58:14 +0900

    ⚙ 環境設定を変更

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -1,6 +1,10 @@
+//@ts-check
+const hp = 'https://konomi.app/';
+const commonCdn = 'https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest';
 const REPO = 'https://cdn.jsdelivr.net/gh/local-bias/kintone-plugin-tab@latest';
+const localhost = 'https://127.0.0.1:5500';
 
-/** @type {import('./src/types/plugin-config').PluginConfig} */
+/** @type { import('./src/types/plugin-config').PluginConfig } */
 module.exports = {
   manifest: {
     base: {
@@ -18,29 +22,20 @@ module.exports = {
         zh: '添加垂直标签',
       },
       icon: 'image/icon.png',
-      homepage_url: {
-        ja: 'https://konomi.app',
-        en: 'https://konomi.app',
-      },
-      desktop: {
-        js: ['https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest/dist/desktop.js'],
-        css: [],
-      },
-      mobile: {
-        js: ['https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest/dist/desktop.js'],
-        css: [],
-      },
+      homepage_url: { ja: hp, en: hp },
+      desktop: { js: [`${commonCdn}/dist/desktop.js`], css: [] },
+      mobile: { js: [`${commonCdn}/dist/desktop.js`], css: [] },
       config: {
-        html: 'html/config.html',
-        js: ['https://cdn.jsdelivr.net/gh/local-bias/kintone-cdn@latest/dist/config.js'],
+        html: 'config.html',
+        js: [`${commonCdn}/dist/config.js`],
         css: [],
         required_params: [],
       },
     },
     dev: {
-      desktop: { js: ['js/desktop.js'] },
-      mobile: { js: ['js/desktop.js'] },
-      config: { js: ['js/config.js'] },
+      desktop: { js: [`${localhost}/dist/dev/desktop/index.js`] },
+      mobile: { js: [`${localhost}/dist/dev/desktop/index.js`] },
+      config: { js: [`${localhost}/dist/dev/config/index.js`] },
     },
     prod: {
       desktop: { js: [`${REPO}/cdn/desktop.js`] },
