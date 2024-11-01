ad6df56c2d629d09a7989e5963084f4dd3520d1c
Ribbit <echo07200503@gmail.com>
2024-11-01 12:44:45 +0900

    📱 モバイル版では表示しない

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -23,7 +23,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '2.9.0',
+      version: '2.9.1',
       type: 'APP',
       name: {
         en: 'vertical tab plugin',
