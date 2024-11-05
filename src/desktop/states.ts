9aca020b941088f30d442f7effeecb58f3ad2046
Ribbit <echo07200503@gmail.com>
2024-11-05 19:05:51 +0900

    🔥 focusAtom (resolve webpack)

--- src/desktop/states.ts
+++ src/desktop/states.ts
@@ -2,14 +2,20 @@ import { GUEST_SPACE_ID } from '@/lib/global';
 import { restorePluginConfig } from '@/lib/plugin';
 import { getAppId, getFormFields, getFormLayout, kintoneAPI } from '@konomi-app/kintone-utilities';
 import { atom } from 'jotai';
-import { focusAtom } from 'jotai-optics';
+// import { focusAtom } from 'jotai-optics';
 
 export const kintoneEventAtom = atom<kintoneAPI.js.EventType | null>(null);
 
 export const kintoneAppAtom = atom(getAppId());
 
 export const pluginConfigAtom = atom(restorePluginConfig());
-export const pluginConditionsAtom = focusAtom(pluginConfigAtom, (s) => s.prop('conditions'));
+// export const pluginConditionsAtom = focusAtom(pluginConfigAtom, (s) => s.prop('conditions'));
+export const pluginConditionsAtom = atom(
+  (get) => get(pluginConfigAtom).conditions,
+  (_, set, newValue: Plugin.Condition[]) => {
+    set(pluginConfigAtom, (prev) => ({ ...prev, conditions: newValue }));
+  }
+);
 
 export const tabIndexAtom = atom(0);
 
