74c3ff446cc6d2563bb5ba106f70dffc930e95cf
Ribbit <echo07200503@gmail.com>
2025-01-19 10:31:40 +0900

    ♻ recoil -> Jotai

--- src/config/states/kintone.ts
+++ src/config/states/kintone.ts
@@ -1,96 +1,79 @@
 import { GUEST_SPACE_ID } from '@/lib/global';
 import { flatLayout } from '@/lib/kintone-api';
 import { getAppId, getFormFields, getFormLayout, kintoneAPI } from '@konomi-app/kintone-utilities';
-import { selector } from 'recoil';
+import { atom } from 'jotai';
 
-const PREFIX = `kintone`;
+export const appFieldsAtom = atom<Promise<kintoneAPI.FieldProperty[]>>(async () => {
+  const app = getAppId();
+  if (!app) {
+    throw new Error('アプリのフィールド情報が取得できませんでした');
+  }
 
-export const appFieldsState = selector<kintoneAPI.FieldProperty[]>({
-  key: 'AppFields',
-  get: async () => {
-    const app = getAppId();
-    if (!app) {
-      throw new Error('アプリのフィールド情報が取得できませんでした');
-    }
+  const { properties } = await getFormFields({
+    app,
+    preview: true,
+    guestSpaceId: GUEST_SPACE_ID,
+    debug: process.env.NODE_ENV === 'development',
+  });
 
-    const { properties } = await getFormFields({
-      app,
-      preview: true,
-      guestSpaceId: GUEST_SPACE_ID,
-      debug: process.env.NODE_ENV === 'development',
-    });
+  const values = Object.values(properties);
 
-    const values = Object.values(properties);
-
-    return values.sort((a, b) => a.label.localeCompare(b.label, 'ja'));
-  },
+  return values.sort((a, b) => a.label.localeCompare(b.label, 'ja'));
 });
 
-export const appLayoutState = selector<kintoneAPI.Layout>({
-  key: `${PREFIX}appLayoutState`,
-  get: async () => {
-    const app = getAppId();
-    if (!app) {
-      throw new Error('アプリのフィールド情報が取得できませんでした');
-    }
+export const appLayoutAtom = atom<Promise<kintoneAPI.Layout>>(async () => {
+  const app = getAppId();
+  if (!app) {
+    throw new Error('アプリのフィールド情報が取得できませんでした');
+  }
 
-    const { layout } = await getFormLayout({
-      app,
-      preview: true,
-      guestSpaceId: GUEST_SPACE_ID,
-      debug: process.env.NODE_ENV === 'development',
-    });
-    return layout;
-  },
+  const { layout } = await getFormLayout({
+    app,
+    preview: true,
+    guestSpaceId: GUEST_SPACE_ID,
+    debug: process.env.NODE_ENV === 'development',
+  });
+  return layout;
 });
 
-export const appLabelsState = selector<string[]>({
-  key: `${PREFIX}appLabelsState`,
-  get: ({ get }) => {
-    const layout = get(appLayoutState);
+export const appLabelsAtom = atom<Promise<string[]>>(async (get) => {
+  const layout = await get(appLayoutAtom);
 
-    const fields = flatLayout(layout);
+  const fields = flatLayout(layout);
 
-    const labels = fields.filter((field) => field.type === 'LABEL') as kintoneAPI.layout.Label[];
+  const labels = fields.filter((field) => field.type === 'LABEL') as kintoneAPI.layout.Label[];
 
-    const parser = new DOMParser();
+  const parser = new DOMParser();
 
-    const texts = labels
-      .map(({ label }) => {
-        const doc = parser.parseFromString(label, 'text/html');
-        return doc.body.textContent;
-      })
-      .filter((text) => !!text);
+  const texts = labels
+    .map(({ label }) => {
+      const doc = parser.parseFromString(label, 'text/html');
+      return doc.body.textContent;
+    })
+    .filter((text) => !!text);
 
-    return texts as string[];
-  },
+  return texts as string[];
 });
 
-export const appGroupsState = selector<kintoneAPI.layout.Group[]>({
-  key: `${PREFIX}appLabelState`,
-  get: ({ get }) => {
-    const layout = get(appLayoutState);
+export const appGroupsAtom = atom<Promise<kintoneAPI.layout.Group[]>>(async (get) => {
+  const layout = await get(appLayoutAtom);
 
-    const groups = Object.values(layout).reduce<kintoneAPI.layout.Group[]>(
-      (acc, value) => (value.type === 'GROUP' ? [...acc, value] : acc),
-      []
-    );
+  const groups = Object.values(layout).reduce<kintoneAPI.layout.Group[]>(
+    (acc, value) => (value.type === 'GROUP' ? [...acc, value] : acc),
+    []
+  );
 
-    return groups;
-  },
+  return groups;
 });
 
-export const appSpacesState = selector<kintoneAPI.layout.Spacer[]>({
-  key: `${PREFIX}appSpacesState`,
-  get: ({ get }) => {
-    const layout = get(appLayoutState);
+export const appSpacesState = atom<Promise<kintoneAPI.layout.Spacer[]>>(async (get) => {
+  const layout = await get(appLayoutAtom);
 
-    const fields = flatLayout(layout);
+  const fields = flatLayout(layout);
 
-    const spaces = fields.filter((field) => field.type === 'SPACER') as kintoneAPI.layout.Spacer[];
+  const spaces = fields.filter((field) => field.type === 'SPACER') as kintoneAPI.layout.Spacer[];
 
-    const filtered = spaces.filter((space) => space.elementId);
+  const filtered = spaces.filter((space) => space.elementId);
 
-    return filtered;
-  },
+  return filtered;
 });
